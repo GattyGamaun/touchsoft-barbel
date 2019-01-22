c0f17c223902f9df493b29b2afbde5bcc1416899
gattygamaun <gattygamaun@gmail.com>
2019-01-22 14:13:12 +0300

    fix: change auto generated array to hand input

--- src/Barbel.java
+++ src/Barbel.java
@@ -1,46 +1,42 @@
 import java.util.Arrays;
-import java.util.Random;
 
-public class Barbel {
+class Barbel {
   private int[] plates;
-  private int maxPlateWeight;
   private int maxBarbelWeight;
-  private int quantityPlates;
 
-  void initializeArray(int maxPlateWeight, int quantityPlates) {
-    if (maxPlateWeight > 0 && maxPlateWeight < 21) {
-      this.maxPlateWeight = maxPlateWeight;
-      if (quantityPlates > 1 && quantityPlates <= 1000) {
-        this.quantityPlates = quantityPlates;
-        plates = new int[this.quantityPlates];
-        for (int i = 0; i < plates.length; i++) {
-          int weight = getRandomNumber();
-          plates[i] = weight;
-        }
-      } else throw new Error("Choose quantity of plates from 2 to 1000");
-    } else throw new Error("Choose max plate weight from 1 to 20");
-  }
-
-  int getMaxBarbelWeight() {
-    maxBarbelWeight = 0;
-    for (int i : plates) {
-      maxBarbelWeight += i;
+  Barbel(int[] plates) {
+    for (int i = 0; i < plates.length; i++) {
+      if (plates[i] > 0 && plates[i] < 21) {
+        if (plates.length < 1000) {
+          this.plates = plates;
+        } else throw new Error("Choose quantity of plates from 2 to 1000");
+      } else throw new Error("Choose max plate weight from 1 to 20");
     }
-    return maxBarbelWeight;
   }
 
-  private int getRandomNumber() {
-    Random random = new Random();
-    return 1 + random.nextInt(maxPlateWeight);
+  int getMaxWeight() {
+    maxBarbelWeight = 0;
+    for (int i : this.plates) {
+      maxBarbelWeight += i;
+      }
+    if (maxBarbelWeight > 10000) {
+      throw new Error("You can not take this weight");
+    } else {
+      return maxBarbelWeight;
+    }
   }
 
   int[] correctWeight() {
+    int extraPlate;
     if (maxBarbelWeight % 2 != 0) {
       Arrays.sort(plates);
       maxBarbelWeight = 0;
       for (int i = 0; i < plates.length; i++) {
         if(plates[i] % 2 != 0) {
+          extraPlate = plates[i];
           plates[i] = 0;
+          System.out.println("You can not use this plate of " + extraPlate + " pound/s if " +
+              "\nyou want to save balance");
         }
         break;
       }
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,17 @@ import java.util.Arrays;
 public class Main {
   public static void main(String[] args) {
 
-    //Make instance of class
-    Barbel barbel = new Barbel();
-
     //Initialize array of plates;
-    //Set max weight of the plate and the quantity
-    barbel.initializeArray(20, 10);
+    int[] initWeights = {2,3,1,1,10,13,20,3};
+
+    //Make instance of class
+    Barbel barbel = new Barbel(initWeights);
+
+    //Calculate max weight
+    int maxWeight = barbel.getMaxWeight();
 
     //Print max weight of the barbel
-    System.out.println("The max barbel weight is " + barbel.getMaxBarbelWeight());
+    System.out.println("The max barbel weight is " + maxWeight);
 
     //Print array of plates
     int[] plates = barbel.getPlates();
@@ -22,7 +24,8 @@ public class Main {
     System.out.println("Corrected Plates: " + Arrays.toString(correctedPlates));
 
     //Print corrected max weight of the barbel
-    System.out.println("The corrected max barbel weight is " + barbel.getMaxBarbelWeight());
+    int maxCorrectedWeight = barbel.getMaxWeight();
+    System.out.println("The corrected max barbel weight is " + maxCorrectedWeight);
 
   }
 }
